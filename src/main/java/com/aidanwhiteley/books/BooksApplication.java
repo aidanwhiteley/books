a2150d8a43348d20eee2d4638908604bfb784b03
Aidan Whiteley <github@aidanwhiteley.com>
2017-12-27 12:17:39 +0000

    Initial commit of shell project

---
+++ src/main/java/com/aidanwhiteley/books/BooksApplication.java
@@ -0,0 +1,12 @@
+package com.aidanwhiteley.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+}

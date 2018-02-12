4861552b18aebb7327d3f4f9cfac0d06fa13b6b4
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2018-02-12 01:57:08 +0000

    Starting to work on Actuator end points

--- src/main/java/com/aidanwhiteley/books/BooksApplication.java
+++ src/main/java/com/aidanwhiteley/books/BooksApplication.java
@@ -1,5 +1,12 @@
 package com.aidanwhiteley.books;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +22,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class BooksApplication extends WebMvcConfigurerAdapter {

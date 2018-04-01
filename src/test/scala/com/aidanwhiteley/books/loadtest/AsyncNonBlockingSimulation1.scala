4fc2ac93ec506157ec7354a4d2bb933a77a22838
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2018-04-01 23:36:52 +0100

    Adding base files fo rGatling testing

---
+++ src/test/scala/com/aidanwhiteley/books/loadtest/AsyncNonBlockingSimulation1.scala
@@ -0,0 +1,43 @@
+package com.aidanwhiteley.books.loadtest
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+ 
+class AsyncNonBlockingSimulation1 extends Simulation {
+
+    val rampUpTimeSecs = 20
+    val testTimeSecs   = 120
+    val noOfUsers      = 10 
+    val minWaitMs      = 1000 milliseconds
+    val maxWaitMs      = 3000 milliseconds
+    val baseName     = "async-non-blocking"
+    val requestName  = baseName + "-request"
+    val scenarioName = baseName + "-scenario"
+
+    val httpProtocol = http
+        .baseURL("http://localhost:8080")
+        .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
+
+    val uri1 = "127.0.0.1"
+    
+    val http_headers = Map(
+        "Accept-Encoding" -> "gzip,deflate",
+        "Content-Type" -> "text/json;charset=UTF-8",
+        "Keep-Alive" -> "115")
+
+    val scn = scenario(scenarioName)
+        .during(testTimeSecs) {
+      exec(
+        http(requestName)
+          .get("/async")
+          .headers(http_headers)
+          .check(status.is(200), regex(""""status":true""") )
+      )
+      .pause(minWaitMs, maxWaitMs)
+    }
+
+    setUp(scn.inject(rampUsers(noOfUsers) over (rampUpTimeSecs seconds))).protocols(httpProtocol)
+}

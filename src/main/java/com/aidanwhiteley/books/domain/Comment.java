8a3122be890602c3bc162289072535c6b3a5ff57
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2018-04-16 21:32:46 +0100

    WiP on Mongo data auditing for sub-documents

--- src/main/java/com/aidanwhiteley/books/domain/Comment.java
+++ src/main/java/com/aidanwhiteley/books/domain/Comment.java
@@ -1,22 +1,35 @@
 package com.aidanwhiteley.books.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.*;
-import org.hibernate.validator.constraints.Length;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
+import static com.aidanwhiteley.books.domain.User.Role.ROLE_ADMIN;
+import static com.aidanwhiteley.books.domain.User.Role.ROLE_EDITOR;
+import static com.aidanwhiteley.books.domain.User.Role.ROLE_USER;
 
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static com.aidanwhiteley.books.domain.User.Role.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Transient;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
@@ -24,73 +37,91 @@ import static com.aidanwhiteley.books.domain.User.Role.*;
 @ToString
 public class Comment implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Comment.class);
 
-    // Mandatory but not marked as @NotNull as set by controller
-    private Owner owner;
+	// Mandatory but not marked as @NotNull as set by controller
+	private Owner owner;
 
-    @Id
-    private String id = UUID.randomUUID().toString();
+	@Id
+	private String id = UUID.randomUUID().toString();
 
-    @NotNull
-    @Length(min = 1, max = 1000)
-    private String commentText;
+	@NotNull
+	@Length(min = 1, max = 1000)
+	private String commentText;
 
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @NotNull
-    private LocalDateTime entered = LocalDateTime.now();
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@NotNull
+	private LocalDateTime entered = LocalDateTime.now();
 
-    private boolean deleted = false;
+	@CreatedBy
+	private Owner createdBy;
 
-    private String deletedBy;
+	@CreatedDate
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	private LocalDateTime createdDateTime;
 
-    // The following  transient field is intended as a "helper" to enable the client
-    // side to create links to functionality that will pass the server side method level security.
-    @Transient
-    @Setter(AccessLevel.NONE)
-    private boolean allowDelete;
+	@LastModifiedBy
+	private Owner lastModifiedBy;
 
-    @SuppressWarnings("WeakerAccess")
-    public Comment() {
-    }
+	@LastModifiedDate
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	private LocalDateTime lastModifiedDateTime;
 
-    public Comment(String commentText, Owner owner) {
-        this.commentText = commentText;
-        this.owner = owner;
-    }
-    
-    public Comment(String commentText, Owner owner, LocalDateTime entered) {
-        this.commentText = commentText;
-        this.owner = owner;
-        this.entered = entered;
-    }
+	private boolean deleted = false;
 
-    public boolean isOwner(User user) {
-        return (user.getAuthenticationServiceId().equals(this.owner.getAuthenticationServiceId()) &&
-                user.getAuthProvider() == this.owner.getAuthProvider());
-    }
+	private String deletedBy;
 
-    public void setPermissionsAndContentForUser(User user) {
-        this.allowDelete = false;
+	// The following transient field is intended as a "helper" to enable the
+	// client
+	// side to create links to functionality that will pass the server side
+	// method level security.
+	@Transient
+	@Setter(AccessLevel.NONE)
+	private boolean allowDelete;
 
-        if (null == user || user.getHighestRole() == ROLE_USER) {
-            this.deletedBy = "";
-        } else if (user.getHighestRole() == ROLE_EDITOR) {
-            if (isOwner(user)) {
-                this.allowDelete = true;
-            }
-        } else if (user.getHighestRole() == ROLE_ADMIN) {
-            this.allowDelete = true;
-        } else {
-            LOGGER.error("Unexpected user role found. This is a code logic error");
-            throw new IllegalStateException("Unable to set content in a Comment appropriate for user");
-        }
+	@SuppressWarnings("WeakerAccess")
+	public Comment() {
+	}
 
-        if (owner != null) {
-            owner.setPermissionsAndContentForUser(user);
-        }
-    }
+	public Comment(String commentText, Owner owner) {
+		this.commentText = commentText;
+		this.owner = owner;
+	}
+
+	public Comment(String commentText, Owner owner, LocalDateTime entered) {
+		this.commentText = commentText;
+		this.owner = owner;
+		this.entered = entered;
+	}
+
+	public boolean isOwner(User user) {
+		return (user.getAuthenticationServiceId().equals(this.owner.getAuthenticationServiceId())
+				&& user.getAuthProvider() == this.owner.getAuthProvider());
+	}
+
+	public void setPermissionsAndContentForUser(User user) {
+		this.allowDelete = false;
+
+		if (null == user || user.getHighestRole() == ROLE_USER) {
+			this.deletedBy = "";
+		} else if (user.getHighestRole() == ROLE_EDITOR) {
+			if (isOwner(user)) {
+				this.allowDelete = true;
+			}
+		} else if (user.getHighestRole() == ROLE_ADMIN) {
+			this.allowDelete = true;
+		} else {
+			LOGGER.error("Unexpected user role found. This is a code logic error");
+			throw new IllegalStateException("Unable to set content in a Comment appropriate for user");
+		}
+
+		if (owner != null) {
+			owner.setPermissionsAndContentForUser(user);
+		}
+	}
 }

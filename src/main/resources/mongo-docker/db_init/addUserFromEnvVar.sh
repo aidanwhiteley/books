ea0e837737fc7062e6627e4336d741cb8ac5d0ce
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2019-11-30 23:31:15 +0000

    Docker WiP

--- src/main/resources/mongo-docker/db_init/addUserFromEnvVar.sh
+++ src/main/resources/mongo-docker/db_init/addUserFromEnvVar.sh
@@ -3,13 +3,13 @@
 # If editing, ensure file has unix style line endings - use dos2unix after editing on Windows
 
 mongo <<EOF
-use admin
+use "$MONGO_INITDB_DB_NAME"
 db.createUser({
   user:  "$MONGO_INITDB_APP_USERNAME",
   pwd: "$MONGO_INITDB_APP_PASSWORD",
   roles: [{
     role: 'readWrite',
-    db: 'books-container-demo-no-auth'
+    db: "$MONGO_INITDB_DB_NAME"
   }]
 })
 EOF

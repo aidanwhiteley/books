f0c5ffe364f549bfd6ada3faff5f2d2fa61f7bca
aidanwhiteley <github@aidanwhiteley.com>
2022-01-06 21:19:50 +0000

    Prootocal independent URL for OpenAPI data

--- src/main/java/com/aidanwhiteley/books/controller/config/OpenApiConfig.java
+++ src/main/java/com/aidanwhiteley/books/controller/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,7 @@ public class OpenApiConfig {
         contact.setUrl("https://aidanwhiteley.com");
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .components(new Components())
                 .info(new Info().title("CloudyBookClub read only API").version(appVersion).contact(contact)
                         .license(new License().name("Apache 2.0").url("https://github.com/aidanwhiteley/books")));

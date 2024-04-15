e4d7dc45860494158707e60cafbdbc1f4f9db105
Aidan Whiteley <aidan.whiteley@gmail.com>
2024-04-15 12:08:38 +0100

    Suppress unwanted logging in a test

--- src/test/java/com/aidanwhiteley/books/controller/config/HttpCookieOAuth2AuthorizationRequestRepositoryTest.java
+++ src/test/java/com/aidanwhiteley/books/controller/config/HttpCookieOAuth2AuthorizationRequestRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.aidanwhiteley.books.controller.config;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import com.aidanwhiteley.books.controller.exceptions.JwtAuthAuzException;
+import com.aidanwhiteley.books.util.JwtAuthenticationUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.LoggerFactory;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
@@ -76,10 +80,15 @@ class HttpCookieOAuth2AuthorizationRequestRepositoryTest {
 						authorizationUri(DUMMY_TEXT_NOT_TESTED).build();
 
 		ObjectMapper om = mock(ObjectMapper.class);
-		when(om.writeValueAsString(any())).thenThrow(new JsonProcessingException("Dummy message for write") {});
-		when(om.readValue(anyString(), eq(OAuth2AuthorizationRequest.class))).thenThrow(new JsonProcessingException("Dummy message for read") {});
+		when(om.writeValueAsString(any())).thenThrow(new JsonProcessingException("This is an expected exception for this test") {});
+		when(om.readValue(anyString(), eq(OAuth2AuthorizationRequest.class))).
+				thenThrow(new JsonProcessingException("This is another expected exception for this test") {});
 
 		HttpCookieOAuth2AuthorizationRequestRepository repo = new HttpCookieOAuth2AuthorizationRequestRepository(om);
+
+		// We dont want expected exception logs cluttering up test logs
+		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+		context.getLogger(HttpCookieOAuth2AuthorizationRequestRepository.class).setLevel(Level.valueOf("OFF"));
 
 		assertThrows(JwtAuthAuzException.class, () ->
 				repo.saveAuthorizationRequest(authorizationRequest, request, response));
@@ -88,6 +97,8 @@ class HttpCookieOAuth2AuthorizationRequestRepositoryTest {
 		request.setCookies(new Cookie[]{aCookie});
 		assertThrows(JwtAuthAuzException.class, () ->
 				repo.loadAuthorizationRequest(request));
+
+		context.getLogger(HttpCookieOAuth2AuthorizationRequestRepository.class).setLevel(Level.valueOf("WARN"));
 
 		verify(om, times(1)).writeValueAsString(authorizationRequest);
 	}

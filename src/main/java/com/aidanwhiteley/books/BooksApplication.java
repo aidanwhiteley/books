cfb01a7364e23495ecc0a1252e4c42a8c6fec1e8
Aidan Whiteley <aidan.whiteley@gmail.com>
2024-07-31 11:54:56 +0100

    Updated dependencies - v0.41.0-RELEASE
    Included about a dozen depdency upgrades

--- src/main/java/com/aidanwhiteley/books/BooksApplication.java
+++ src/main/java/com/aidanwhiteley/books/BooksApplication.java
@@ -3,14 +3,18 @@ package com.aidanwhiteley.books;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 @EnableMongoAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class BooksApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
--- src/main/java/com/aidanwhiteley/books/controller/BookController.java
+++ src/main/java/com/aidanwhiteley/books/controller/BookController.java
@@ -11,11 +11,7 @@ import com.aidanwhiteley.books.service.dtos.SummaryStats;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;

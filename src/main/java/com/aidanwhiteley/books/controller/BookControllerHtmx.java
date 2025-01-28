2d2ebdf27e40211df4f4e75bed84f30a5f39ad35
Aidan Whiteley <aidan.whiteley@gmail.com>
2025-01-28 18:33:21 +0000

    Added Thymeleaf / Htmx error handling

--- src/main/java/com/aidanwhiteley/books/controller/BookControllerHtmx.java
+++ src/main/java/com/aidanwhiteley/books/controller/BookControllerHtmx.java
@@ -22,8 +22,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.UncategorizedMongoDbException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -31,12 +31,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import org.springframework.data.mongodb.UncategorizedMongoDbException;
-
-import java.net.URI;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -486,66 +483,77 @@ public class BookControllerHtmx {
     // so that we can return HTML views for any errors from this controller.
 
     @ExceptionHandler(UncategorizedMongoDbException.class)
-    public String handleInMemoryMongoFulLTextSearchException(UncategorizedMongoDbException ex, Model model, Principal principal) {
+    public String handleInMemoryMongoFullTextSearchException(UncategorizedMongoDbException ex, Model model,
+                                                             Principal principal, WebRequest request) {
         LOGGER.error("An UncategorizedMongoDbException occurred. This is normally expected when running in " +
                 "development mode when trying to use the Search as full text indexes aren't supported by " +
                 "the in memory fake Mongo. However, as it is just possible that it could happen for other " +
                 "reasons, the full stack trace is logged", ex);
         String description = "Search doesn't work when running against the in-memory Mongo " +
                 "used in development because full text indexes are not supported in that implementation.";
-        return addAttributesToErrorPage(description, "e-mongo-uncategorized", model, principal);
+        return addAttributesToErrorPage(description, "e-mongo-uncategorized", model, principal, request);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({IllegalArgumentException.class, NumberFormatException.class, MethodArgumentTypeMismatchException.class,
             MethodArgumentNotValidException.class})
-    public String handleIllegalArgumentException(Exception ex, Model model, Principal principal) {
+    public String handleIllegalArgumentException(Exception ex, Model model, Principal principal, WebRequest request) {
         LOGGER.error("An unacceptable input was received. Either this is an application error or someone manually sending incorrect parameters", ex);
         String description = "Sorry - the values sent to the application are not acceptable.";
-        return addAttributesToErrorPage(description, "e-400", model, principal);
+        return addAttributesToErrorPage(description, "e-400", model, principal, request);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public String handleNotFoundException(NotFoundException ex, Model model, Principal principal) {
+    public String handleNotFoundException(NotFoundException ex, Model model, Principal principal, WebRequest request) {
         LOGGER.error("The application couldn't find the resource requested - {}", ex.getMessage(), ex);
         String description = "Sorry - the application could not find what you wanted";
-        return addAttributesToErrorPage(description, "e-404", model, principal);
+        return addAttributesToErrorPage(description, "e-404", model, principal, request);
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(NotAuthorisedException.class)
-    public String handleNotAuthorisedException(NotAuthorisedException ex, Model model, Principal principal) {
+    public String handleNotAuthorisedException(NotAuthorisedException ex, Model model, Principal principal, WebRequest request) {
         LOGGER.error("An attempt was made to access a protected resource without the required authorisation - {}", ex.getMessage(), ex);
         String description = "Sorry - you are not authorised to access this functionality";
-        return addAttributesToErrorPage(description, "e-401", model, principal);
+        return addAttributesToErrorPage(description, "e-401", model, principal, request);
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(JwtAuthAuzException.class)
-    public String handleJwtAuthAuzException(JwtAuthAuzException ex, Model model, Principal principal) {
+    public String handleJwtAuthAuzException(JwtAuthAuzException ex, Model model, Principal principal, WebRequest request) {
         LOGGER.error("There was a problem with the JWT token process - {}", ex.getMessage(), ex);
         String description = "Sorry - there was problem with processing your logon token";
-        return addAttributesToErrorPage(description, "e-401", model, principal);
+        return addAttributesToErrorPage(description, "e-401", model, principal, request);
     }
 
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
-    public String handleAccessDeniedException(AccessDeniedException ex, Model model, Principal principal) {
+    public String handleAccessDeniedException(AccessDeniedException ex, Model model, Principal principal, WebRequest request) {
         LOGGER.error("An attempt was made to access a protected resource without the required permission - {}", ex.getMessage(), ex);
         String description = "Sorry - you are not permitted to access this functionality";
-        return addAttributesToErrorPage(description, "e-403", model, principal);
+        return addAttributesToErrorPage(description, "e-403", model, principal, request);
     }
 
     // Exception handler of last resort!
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex, Model model, Principal principal) {
+    public String handleException(Exception ex, Model model, Principal principal, WebRequest request) {
         LOGGER.error("An unhandled exception was caught be the exception handler of last resort - {}", ex.getMessage(), ex);
         String description = "Sorry - an unexpected problem occurred in the application.";
-        return addAttributesToErrorPage(description, "e-500", model, principal);
+        return addAttributesToErrorPage(description, "e-500", model, principal, request);
     }
 
-    private String addAttributesToErrorPage(String description, String code, Model model, Principal principal) {
+    private String addAttributesToErrorPage(String description, String code, Model model, Principal principal, WebRequest request) {
         model.addAttribute("description", description);
         model.addAttribute("code", code);
         model.addAttribute("dateTime", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
         addUserToModel(principal, model);
-        return "error";
-    }
 
+        if (request.getHeader("hx-request") != null && request.getHeader("Hx-request").equals("true")) {
+            return "error :: cloudy-error-detail";
+        } else {
+            return "error";
+        }
+    }
 }

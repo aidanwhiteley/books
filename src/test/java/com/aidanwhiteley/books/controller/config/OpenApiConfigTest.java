f6cc0c661f22f8acab0cc5ccca90a2831003bae0
Aidan Whiteley <aidan.whiteley@gmail.com>
2025-04-06 16:08:30 +0100

    handle logon from both cloudy and spa.cloudy

--- src/test/java/com/aidanwhiteley/books/controller/config/OpenApiConfigTest.java
+++ src/test/java/com/aidanwhiteley/books/controller/config/OpenApiConfigTest.java
@@ -1,6 +1,7 @@
 package com.aidanwhiteley.books.controller.config;
 
 import com.aidanwhiteley.books.util.IntegrationTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;

809ebced136c7581ed40f165ad996f749174c9fe
Aidan Whiteley <github@aidanwhiteley.com>
2025-12-31 21:18:52 +0000

    Working through Sonar complaints

--- src/main/java/com/aidanwhiteley/books/repository/GoogleBooksDaoSync.java
+++ src/main/java/com/aidanwhiteley/books/repository/GoogleBooksDaoSync.java
@@ -32,15 +32,8 @@ public class GoogleBooksDaoSync {
         this.googleBooksRestTemplate = new RestTemplate();
     }
 
-    // Commenting this out to ease the migration to spring Boot 4 after which we'll convert
+    // Commenting out the previous init() method to ease the migration to spring Boot 4 after which we'll convert
     // to using RestClient anyway
-//    @PostConstruct
-//    public void init() {
-//        // Using a PostConstruct as we need the bean initialised to be able to
-//        // access the configurable connect and read timeouts
-//        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-//        this.googleBooksRestTemplate = buildRestTemplate(restTemplateBuilder);
-//    }
 
     public BookSearchResult searchGoogleBooksByTitleAndAuthor(String title, String author) {
 
@@ -92,10 +85,4 @@ public class GoogleBooksDaoSync {
             throw e;
         }
     }
-
-//    private RestTemplate buildRestTemplate(RestTemplateBuilder builder) {
-//
-//        return builder.connectTimeout(Duration.ofMillis(googleBooksApiConfig.getConnectTimeout())).
-//                readTimeout(Duration.ofMillis(googleBooksApiConfig.getReadTimeout())).build();
-//    }
 }
